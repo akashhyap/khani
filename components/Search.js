7549602417a835091afb5744fbd6aec5f2994fae
Akashdeep <akash.kashhyap@gmail.com>
2023-02-21 12:43:59 +0530

    vercel error

--- components/Search.js
+++ components/Search.js
@@ -1,6 +1,6 @@
 import Link from "next/link";
 import { useState } from "react";
-import searchContent from "../pages/lib/_search";
+import searchContent from "./FetchSearch";
 
 export default function Search() {
   const [searchQuery, setSearchQuery] = useState("");
